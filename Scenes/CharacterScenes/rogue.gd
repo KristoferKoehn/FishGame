924ef6502b55d443769f67545d67c1886847fa12
Arlo Jones <Arloe.jones3@gmail.com>
2024-04-11 23:19:17 -0700

    Got camera to pan

--- Scenes/CharacterScenes/rogue.gd
+++ Scenes/CharacterScenes/rogue.gd
@@ -1,8 +1,16 @@
 extends CharacterBody3D
 
+var camera: Camera3D = null
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const TURN_SPEED = 10
+const locomotionBlendPositionVector = Vector2.ZERO
+const locomotionBlendPositionSpeed = 3.5
+
+
+func _ready() -> void:
+	camera = get_node("../Camera3D") as Camera3D
 
 
 func _physics_process(delta: float) -> void:
@@ -16,11 +24,13 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir := Input.get_vector("left", "right", "forward", "back")
+	var direction := (camera.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		var targetTransform = transform.looking_at(global_position - direction)
+		transform = transform.interpolate_with(targetTransform, TURN_SPEED * delta)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
