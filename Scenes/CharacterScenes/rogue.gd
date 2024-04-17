a12fd7b2b5c785fc2796afd81376a3c68d3774ad
kkoehn <kristofer.koehn@wsu.edu>
2024-04-16 22:39:35 -0700

    wahoo

--- Scenes/CharacterScenes/rogue.gd
+++ Scenes/CharacterScenes/rogue.gd
@@ -8,19 +8,13 @@ const TURN_SPEED = 10
 const locomotionBlendPositionVector = Vector2.ZERO
 const locomotionBlendPositionSpeed = 3.5
 
-
 func _ready() -> void:
 	camera = get_node("../Camera3D") as Camera3D
-
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -30,6 +24,9 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 		var targetTransform = transform.looking_at(global_position - direction)
+		targetTransform.basis.x.x = 0.0
+		targetTransform.basis.z.z = 0.0
+		
 		transform = transform.interpolate_with(targetTransform, TURN_SPEED * delta)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
--- Scenes/camera_3d.gd
+++ Scenes/camera_3d.gd
@@ -14,9 +14,12 @@ func _process(delta: float) -> void:
 	if character == null:
 		character = get_node("../Rogue")
 	
-	if cameraTransform1 == null:
-		var cameraNode: Node3D = get_node("../CameraPosition1")
-		cameraTransform1 = cameraNode.transform
-	else:
-		transform = transform.interpolate_with(cameraTransform1, 0.1 * delta)
+	var horizontal_offset = Vector3(character.global_position.x, 0 , character.global_position.z)
+
+	global_position = horizontal_offset + Vector3(-10,4,0)
+	#var cameraNode: Node3D = get_node("../CameraPosition1")
+	#var cameraNode: Node3D = character.get_node("CameraNode")
+	
+	#cameraTransform1 = cameraNode.global_transform
+	#transform = transform.interpolate_with(cameraTransform1, 1 * delta)
 		
