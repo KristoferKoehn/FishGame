aacc3b59687c27d6d46023a1c5bdf5c6cb42e633
Arlo Jones <67977487+Aejace@users.noreply.github.com>
2024-04-22 21:58:56 -0700

    Added art

--- Scenes/GameScenes/FishingMiniGame/character_body_2d.gd
+++ Scenes/GameScenes/FishingMiniGame/character_body_2d.gd
@@ -1,20 +1,28 @@
 extends CharacterBody2D
 
-var SPEED = 600.0
-var TURN_SPEED = 3
+var speed = 600.0
+var defaultSpeed = 600
+var sprintSpeed = 900
+var turnSpeed = 3
 
 func _ready() -> void:
 	position = get_viewport_rect().size / 2
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("left", "right", "forward", "back")
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.y = direction.y * SPEED
-		var targetTransform = transform.looking_at(global_position - direction)
-		transform = transform.interpolate_with(targetTransform, TURN_SPEED * delta)
+	
+	if Input.is_action_pressed("sprint"):
+		speed = sprintSpeed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+		speed = defaultSpeed
+	
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.y = direction.y * speed
+		var targetTransform = transform.looking_at(global_position - direction)
+		transform = transform.interpolate_with(targetTransform, turnSpeed * delta)
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.y = move_toward(velocity.y, 0, speed)
 	
 	move_and_slide()
