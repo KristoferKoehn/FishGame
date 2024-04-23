25f4131ad78e564eea7cb0cf7ce9ad4428afc1ad
Arlo Jones <67977487+Aejace@users.noreply.github.com>
2024-04-23 02:18:36 -0700

    Added catch bar and timmer, started on chest UI

--- Scenes/GameScenes/FishingMiniGame/fishing_mini_game.gd
+++ Scenes/GameScenes/FishingMiniGame/fishing_mini_game.gd
@@ -3,23 +3,29 @@ extends Node2D
 var fish = null
 var hook = null
 var time = 0
-var timeLimit = 10
+var timeLimit = 20
 signal fishCaught(success: bool)
 var distanceCutoff = 200
-var successTimeLimit = 6
+var successTimeLimit = 5
 var successTime = 0
+var catchBar = null
+var countDown = null
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	fish = $Fish
 	hook = $CharacterBody2D/Bobber/Hook
+	catchBar = $ProgressBar
+	countDown = $CountDown
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	time = time + delta
-	if time >= timeLimit:
+	countDown.text = str(int(timeLimit - time))
+	
+	if time >= timeLimit: # TODO: Offer retry
 		# Mission Failed
 		fishCaught.emit(false)
 		queue_free()
@@ -27,6 +33,7 @@ func _process(delta: float) -> void:
 	var distance = abs(hook.global_position - fish.global_position).length()
 	if distance < distanceCutoff:
 		successTime = successTime + delta
+		catchBar.value = successTime / successTimeLimit
 	
 	if successTime >= successTimeLimit:
 		# successful catch
