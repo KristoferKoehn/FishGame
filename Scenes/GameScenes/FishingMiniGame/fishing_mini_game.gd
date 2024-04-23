c330c09020917fedbdd0edb6fa508dc345be8ac0
Arlo Jones <67977487+Aejace@users.noreply.github.com>
2024-04-22 18:55:48 -0700

    Update fishing_mini_game.gd
    Added basic catch mechanic

--- Scenes/GameScenes/FishingMiniGame/fishing_mini_game.gd
+++ Scenes/GameScenes/FishingMiniGame/fishing_mini_game.gd
@@ -1,11 +1,34 @@
 extends Node2D
 
+var fish = null
+var hook = null
+var time = 0
+var timeLimit = 10
+signal fishCaught(success: bool)
+var distanceCutoff = 200
+var successTimeLimit = 6
+var successTime = 0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	fish = $Fish
+	hook = $CharacterBody2D/Bobber/Hook
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	time = time + delta
+	if time >= timeLimit:
+		# Mission Failed
+		fishCaught.emit(false)
+		queue_free()
+	
+	var distance = abs(hook.global_position - fish.global_position).length()
+	if distance < distanceCutoff:
+		successTime = successTime + delta
+	
+	if successTime >= successTimeLimit:
+		# successful catch
+		fishCaught.emit(true)
+		queue_free()
