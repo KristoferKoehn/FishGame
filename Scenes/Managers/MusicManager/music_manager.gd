d6c46f9db7af3ce61e07d4cd96513add4d018391
kkoehn <kristofer.koehn@wsu.edu>
2024-04-28 01:35:09 -0700

    doing music

---
+++ Scenes/Managers/MusicManager/music_manager.gd
@@ -0,0 +1,12 @@
+extends Node
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if TimeManager.get_time() > 20 || TimeManager.get_time() < 8:
+		pass
+	
+
